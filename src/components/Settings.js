f541e7cd659312fea378a33d7f7d57b3acdfebdb
Charity Wanjiku Ngocho <ngochocharity@gmail.com>
2022-07-29 00:41:56 +0300

    creates the settings page

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
+function Settings ({setIsLoggedIn, color, setColor, handleTheme, isDark}){
+    const navigate = useNavigate()
 
-function Settings (){
+    function handleLogout({ setIsLoggedIn}){
+      setIsLoggedIn(false)
+      navigate('/')
+    }
+    function handleDisplay(){
+
+    }
+   const theme = isDark ?'Light Mode':'Dark Mode'
     return(
-        <div>Settings</div>
+        <div style={{background:`${color}`}}>
+            <div className="settings">
+                <center>
+                   <div className="profile"> 
+                      <button onClick={handleDisplay} >Hide
+                      Profile</button>
+                   </div>
+                   <div className="logout">
+                      <button onClick={handleLogout}>Logout</button>
+                   </div>
+                   <div className="theme">
+                   <button onClick={handleTheme}>{theme}</button>
+                   </div>
+                </center>
+            </div>
+        </div>
     )
 }
 export default Settings;
