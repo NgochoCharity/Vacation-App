5b09fd3e64179dea4848515552f7e5b8047f8c74
Charity Wanjiku Ngocho <ngochocharity@gmail.com>
2022-07-28 13:51:44 +0300

    Creates the Routes to the various pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./components/Home";
+import Destinations from "./components/Destinations"
+import Reservation from "./components/Reservation"
+import Login from "./components/Login"
+import Settings from "./components/Settings";
+import {Routes, Route} from "react-router-dom"
+import Navbar from "./components/Navbar";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+     <Routes>
+      <Route exact path = "/Destinations" element= {<Destinations/>} />
+      <Route exact path = "/Reservation" element= {<Reservation/>} />
+      <Route exact path = "/Login" element= {<Login/>} />
+      <Route exact path = "/Settings" element= {<Settings/>} />
+      <Route exact path = "/" element= {<Home />} />
+     </Routes>
     </div>
   );
 }
 
 export default App;
+
