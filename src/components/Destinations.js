485697729aa005eabc65cde5cc7913ee551b8f93
Charity Wanjiku Ngocho <ngochocharity@gmail.com>
2022-07-30 23:27:30 +0300

    creates the destinations page

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import DestinationItem from "./DestinationItem";
 
 
-function Destinations (){
+function Destinations ({color, setColor}){
+    
+    const [items, setItems] = useState([]);
+
+    useEffect(() =>{
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'b1922c70f7msh135d7b1fa27b2efp15d980jsn47af833c4872',
+                'X-RapidAPI-Host': 'airbnb19.p.rapidapi.com'
+            }
+        };
+        
+        fetch('https://airbnb19.p.rapidapi.com/api/v1/searchProperty?category=TAB_8225&totalRecords=10&currency=USD&adults=1', options)
+            .then(response => response.json())
+            .then((response) => {
+                setItems(response.data) 
+            });
+            
+    }, [])
+
+    const displayItems = items.map(destination =>(
+     <DestinationItem  key={destination.id} destination={destination} title={destination.title} rating={destination.starRating} bed={destination.beds} bath={destination.bathrooms} guests={destination.listingGuestLabel} price={destination.accessibilityLabel} images={destination.images[0]}/> 
+      ) )
+        
+    
 return (
-<div>
-    <p>Destinations</p>
+<div className="destinations"style={{background:`${color}`}}>
+    
+         {displayItems}    
+    
 </div>
 )
 }
 
 export default Destinations;
+
+
+
